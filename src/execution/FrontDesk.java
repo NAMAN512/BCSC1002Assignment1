4bf2cff30906b96312613adf3567fb0c5c06da76
NAMAN512 <naman.dulani_cs19@gla.ac.in>
2020-08-30 22:48:45 +0530

    create FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,26 +7,76 @@
 package execution;
 
 import definitions.Book;
+import definitions.Library;
+import definitions.Student;
 
 import java.util.Scanner;
 
 public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK_FOR_ME = 2;
+    private static final int SHOW_ME_ALL_MY_ISSUES_BOOKS = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
-        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n" +
-                "How may I help you today?\n" +
-                "1. Issue a new book for me.\n" +
-                "2. Return a previously issues book for me.\n" +
-                "3. Show me all my issues books.\n" +
-                "4. Exit.");
         Scanner sc = new Scanner(System.in);
+        int Input;
 
-        int n = sc.nextInt();
+        Library libraryObject = new Library();
+        Student studentObject = new Student();
+        Book bookObject = new Book();
 
-        if (n == 1) {
-            Book book = new Book();
-            System.out.println("Book Name :"bookName);
-        } else if (n == 2) {
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?\n");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice: ");
+            Input = sc.nextInt();
+            switch (Input) {
+                case ISSUE_A_NEW_BOOK_FOR_ME:
+                    System.out.println("How many Books you want to issue: ");
+                    int n = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter the name of the books that you want to issue: ");
+                    String[] bookNames = new String[n];
+                    for (int i = 0; i < n; i++) {
+                        bookNames[i] = sc.next();
+                    }
+                    for (String bookName : bookNames) {
+                        System.out.println(bookName);
+                    }
 
-        }
+                    break;
+
+                case RETURN_A_PREVIOUSLY_ISSUED_BOOK_FOR_ME:
+                    System.out.println("How many Books you want to return: ");
+                    int r = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter the name of the books that you want to issue: ");
+                    String[] returnBookName = new String[r];
+                    for (int i = 0; i < r; i++) {
+                        returnBookName[i] = sc.next();
+                    }
+                    for (String bookName : returnBookName) {
+                        System.out.println(bookName);
+                    }
+
+                    break;
+
+                case SHOW_ME_ALL_MY_ISSUES_BOOKS:
+                    System.out.println("Your all issues book names are: ");
+                    sc.nextLine();
+
+                    break;
+
+                default:
+            }
+        } while (Input != EXIT);
+        sc.close();
     }
 }
+
+
