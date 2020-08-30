61bd6ff72b9316c12a22b1f1254dd277d73f96c0
NAMAN512 <naman.dulani_cs19@gla.ac.in>
2020-08-30 21:09:55 +0530

    create equals() and hashCode() method for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -36,4 +36,18 @@ public class Library {
                 "provide=" + Arrays.toString(provide) +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getProvide(), library.getProvide());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getProvide());
+    }
+
 }
